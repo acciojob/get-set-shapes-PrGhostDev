92c676458176e889899cf51814fdc99aeaafe3aa
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-10-26 14:13:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 //complete this code
-class Rectangle {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Square extends Animal {}
+  get species() {
+    return this._species;
+  }
+
+  makeSound() {
+    console.log("Some generic animal sound");
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("Woof");
+  }
+}
+
+class Cat extends Animal {
+  purr() {
+    console.log("Purr");
+  }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
